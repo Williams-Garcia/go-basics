25a4b144c36e20730b0a89a25f5f9754b3c8df53
Williams-Garcia <williams.garcia@mercadolibre.com.mx>
2022-12-28 14:18:32 -0600

    Add control panic in exercise 5

--- error_taxes_five/taxesErrorFive.go
+++ error_taxes_five/taxesErrorFive.go
@@ -3,6 +3,7 @@ package error_taxes_five
 import (
 	"errors"
 	"fmt"
+	"log"
 )
 
 var (
@@ -12,7 +13,8 @@ var (
 
 func Init() {
 	salary := 155000.0
-	hours := 200.0
+	hours := 20.0
+	/* Manejando el error y validando con panic en main
 	result, err := calculateSalaryPerMonthByHours(salary, hours)
 	if err != nil {
 		if errors.Is(err, errSalary) {
@@ -22,8 +24,13 @@ func Init() {
 			panic(err)
 		}
 	}
-	fmt.Printf("Salario inicial: %v\n Salario final: %v\n", salary, result)
+	fmt.Printf("Salario inicial: %v\n Salario final: %v\n", salary, result)*/
 
+	//Por manejo de panic con defer y recover
+	result := calculateSalaryPerMonthByHoursPanic(salary, hours)
+	if result != 0 {
+		fmt.Printf("Salario inicial: %v\n Salario final: %v\n", salary, result)
+	}
 }
 
 func calculateSalaryPerMonthByHours(salary float64, workHours float64) (totalSalary float64, err error) {
@@ -36,4 +43,23 @@ func calculateSalaryPerMonthByHours(salary float64, workHours float64) (totalSal
 	}
 
 	return salary - (salary * 0.10), nil
+}
+
+func calculateSalaryPerMonthByHoursPanic(salary float64, workHours float64) (totalSalary float64) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	if salary <= 150000.0 {
+		panic(fmt.Errorf("%w %v", errSalary, salary))
+	}
+
+	if workHours < 80.0 {
+		panic(fmt.Errorf("%w", errHours))
+	}
+
+	return salary - (salary * 0.10)
 }
